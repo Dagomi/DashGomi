29b36c38b5dcbed004f12c26a7b4f253ad171533
Dagomi <ashkain64@gmail.com>
2015-12-07 16:56:40 +0100

    Add enviorment variables: CPU and Battery

--- workspace/EPSG/src/EPSG.py
+++ workspace/EPSG/src/EPSG.py
@@ -1,12 +1,11 @@
 '''
 Created on Dec 3, 2015
-
 @author: Dagomi
 '''
 #!/usr/bin/env python
 import os
 import gi
-
+import psutil
 #http://www.bok.net/dash/tears_of_steel/cleartext/stream.mpd
 #http://localhost/dash/trik_audio_video/stream.mpd
 '''
@@ -39,67 +38,92 @@ class GTK_Main(object):
         sliders = Gtk.HBox()
         vbox.pack_end(sliders, False, False, 0)
         
-        table = Gtk.Table(5, 2, False)
-
+        table = Gtk.Table(6, 4, False)
         table.set_col_spacings(25)
         sliders.add(table)
-        
-
-        
+    
         #Labels
+        #Sliders
         label_top_left = Gtk.Label(label="Buffer Ocupancy" )
         label_top_right = Gtk.Label(label="Battery Level")
         label_bottom_left = Gtk.Label(label="CPU Load")
         label_bottom_right = Gtk.Label(label="Available Bandwidth")
         
+        #Simulations
+        self.label_Simulation = Gtk.Label(label="Simulation")
+        self.label_Buffer_Sim = Gtk.Label(label="Buffer is 0")
+        self.label_Battery_Sim = Gtk.Label(label="Battery is 0")
+        self.label_CPU_Sim = Gtk.Label(label="CPU is 0")
+        self.label_BW_Sim = Gtk.Label(label="BW is 0")
+        
+        #Enviorment
+        self.label_Real = Gtk.Label(label="Enviorment")
+        self.label_Buffer_Real = Gtk.Label(label="Buffer is 0")
+        self.label_Battery_Real = Gtk.Label(label="Battery is 0")
+        self.label_CPU_Real = Gtk.Label(label="CPU is 0")
+        self.label_BW_Real = Gtk.Label(label="BW is 0")
+
+
+        
         # in the grid:
         # attach the first label in the top left corner   (left_attach,right_attach,top_attach,bottom_attach)
         table.attach(label_top_left, 0, 1, 0, 1)
-   
         table.attach(label_top_right, 1, 2, 0, 1)
         table.attach(label_bottom_left, 0, 1, 3, 4)
         table.attach(label_bottom_right, 1, 2, 3, 4)
         
+        table.attach(self.label_Simulation, 2, 3, 0, 1)        
+        table.attach(self.label_Buffer_Sim, 2, 3, 1, 2)
+        table.attach(self.label_Battery_Sim, 2, 3, 2, 3)
+        table.attach(self.label_CPU_Sim, 2, 3, 4, 5)
+        table.attach(self.label_BW_Sim, 2, 3, 5, 6)
+        
+        table.attach(self.label_Real, 3, 4, 0, 1)        
+        table.attach(self.label_Buffer_Real, 3, 4, 1, 2)
+        table.attach(self.label_Battery_Real, 3, 4, 2, 3)
+        table.attach(self.label_CPU_Real, 3, 4, 4, 5)
+        table.attach(self.label_BW_Real, 3, 4, 5, 6)
+        
         #Togle button Power supply
-        button1 = Gtk.CheckButton(" Power Supply ")
-        button1.connect("toggled", self.on_button_power_supply, "1")
-        table.attach (button1, 1, 2, 1, 2)
+        PowerSupplyButton = Gtk.CheckButton(" Power Supply ")
+        PowerSupplyButton.connect("toggled", self.on_button_power_supply, "1")
+        table.attach (PowerSupplyButton, 1, 2, 1, 2)
 
-        #Slider 1
-        scale1 = Gtk.HScale()
-        scale1.set_range(0, 100)
-        scale1.set_increments(1, 10)
-        scale1.set_digits(0)
-        scale1.set_size_request(50, 35)
-        #scale.connect("value-changed", self.on_changed)
-        table.attach(scale1, 0, 1, 2, 3)
+        #Slider 1 Buffer
+        self.Buffer_Sim = Gtk.HScale()
+        self.Buffer_Sim.set_range(0, 100)
+        self.Buffer_Sim.set_increments(1, 10)
+        self.Buffer_Sim.set_digits(0)
+        self.Buffer_Sim.set_size_request(50, 35)
+        self.Buffer_Sim.connect("value-changed", self.BufferChange)
+        table.attach(self.Buffer_Sim, 0, 1, 2, 3)
    
         #Slider 2
-        scale2 = Gtk.HScale()
-        scale2.set_range(0, 100)
-        scale2.set_increments(1, 10)
-        scale2.set_digits(0)
-        scale2.set_size_request(50, 35)
-        #scale.connect("value-changed", self.on_changed)
-        table.attach(scale2, 0, 1, 4, 5)
+        self.CPU_Sim = Gtk.HScale()
+        self.CPU_Sim.set_range(0, 100)
+        self.CPU_Sim.set_increments(1, 10)
+        self.CPU_Sim.set_digits(0)
+        self.CPU_Sim.set_size_request(50, 35)
+        self.CPU_Sim.connect("value-changed", self.CPUChange)
+        table.attach(self.CPU_Sim, 0, 1, 4, 5)
          
         #Slider 3
-        scale3 = Gtk.HScale()
-        scale3.set_range(0, 100)
-        scale3.set_increments(1, 10)
-        scale3.set_digits(0)
-        scale3.set_size_request(50, 35)
-        #scale.connect("value-changed", self.on_changed)
-        table.attach(scale3, 1, 2,4,5)
+        self.BW_Sim = Gtk.HScale()
+        self.BW_Sim.set_range(0, 100)
+        self.BW_Sim.set_increments(1, 10)
+        self.BW_Sim.set_digits(0)
+        self.BW_Sim.set_size_request(50, 35)
+        self.BW_Sim.connect("value-changed", self.BWChange)
+        table.attach(self.BW_Sim, 1, 2,4,5)
          
         #Slider 4
-        scale4 = Gtk.HScale()
-        scale4.set_range(0, 100)
-        scale4.set_increments(1, 10)
-        scale4.set_digits(0)
-        scale4.set_size_request(50, 35)
-        #scale.connect("value-changed", self.on_changed)
-        table.attach(scale4, 1, 2, 2, 3)
+        self.Battery_Sim = Gtk.HScale()
+        self.Battery_Sim.set_range(0, 100)
+        self.Battery_Sim.set_increments(1, 10)
+        self.Battery_Sim.set_digits(0)
+        self.Battery_Sim.set_size_request(50, 35)
+        self.Battery_Sim.connect("value-changed", self.BatteryChange)
+        table.attach(self.Battery_Sim, 1, 2, 2, 3)
         
         self.entry = Gtk.Entry()
         hbox.add(self.entry)
@@ -174,15 +198,15 @@ class GTK_Main(object):
         bus.connect("sync-message::element", self.on_sync_message)
         
     def open_mpd(self, w):
-        
+        self.enviorment()
         if self.button_open.get_label() == "Play":
                 self.player.set_state(Gst.State.PLAYING)
         #check button value 
         if self.button_open.get_label() == "Open":
         
             #filepath = self.entry.get_text().strip()
-            filepath = 'http://localhost/dash/normal/stream.mpd'
-            
+#             filepath = 'http://localhost/dash/normal/stream.mpd'
+            filepath = 'http://www.bok.net/dash/tears_of_steel/cleartext/stream.mpd'
             if filepath.startswith("http://"):
                 print ('Url OK')
                 self.button_open.set_label("Play")
@@ -259,13 +283,60 @@ class GTK_Main(object):
         else:
             state = "off"
         print("Button", name, "was turned", state)
+        
+    def BufferChange(self, event):
+        self.label_Buffer_Sim.set_text("Buffer is " + str(int(self.Buffer_Sim.get_value())))
+    
+    def BatteryChange(self, event):
+        self.label_Battery_Sim.set_text("Battery is " + str(int(self.Battery_Sim.get_value())))
+    
+    def CPUChange(self, event):
+        self.label_CPU_Sim.set_text("CPU is " + str(int(self.CPU_Sim.get_value())))
+    
+    def BWChange(self, event):
+        self.label_BW_Sim.set_text("BW is " + str(int(self.BW_Sim.get_value())))        
 
-
+    def enviormentCPU (self):
+        psutil.cpu_times()
+        psutil.cpu_percent(interval=0.5)
+        
+    def enviormentBattery (self):
+        BATT_NOW     =   "/sys/class/power_supply/BAT0/charge_now"
+        BATT_FULL    =  "/sys/class/power_supply/BAT0/charge_full_design"
+        BATT_STATUS  =     "/sys/class/power_supply/BAT0/status"    
+    
+        full = open(BATT_FULL, 'r')
+        full_value = full.read()
+        
+        now = open(BATT_NOW, 'r')
+        now_value = now.read()
+        
+        status = open(BATT_STATUS, 'r')
+        status_value = status.read()
+        
+        if 'Charging' in  status_value:
+            statusBattery = 'Charging'
+        
+        elif 'Discharging' in status_value:
+            statusBattery = 'Discharging'
+        
+        elif 'Full' in status_value:
+            statusBattery = ' Full '
+        
+        print full_value
+        print now_value
+        print statusBattery
+        
+        BatteryLoad = (int(now_value) / ( int(full_value)/100))
+          
+        print BatteryLoad
+        full.close()
+        now.close()
+        status.close()
+        
+        
 
 
 if __name__ == "__main__":
     GTK_Main()# run Python Class
     Gtk.main()# run Ui
-
-
-
